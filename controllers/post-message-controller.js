01b22a5ac259d3861bf05e6a934b74c6239875cd
Hugo Iuri <hugoraiuri@gmail.com>
2015-05-26 00:18:36 -0300

    teste de stress com o Jmeter

--- controllers/post-message-controller.js
+++ controllers/post-message-controller.js
@@ -1,4 +1,19 @@
 var Joi = require('joi');
+var mongoose = require('mongoose');
+
+var db = mongoose.connection;
+
+db.on('error', console.error);
+db.once('open', function() {
+
+});
+
+var message = new mongoose.Schema({
+  message: String
+});
+var Message = mongoose.model('message', message);
+
+mongoose.connect('mongodb://localhost:27017/test');
 
 exports.register = function(server, options, next){
   server.route({
@@ -6,7 +21,16 @@ exports.register = function(server, options, next){
     path: '/api/v1/message',
     handler: function(request, reply){
 
-      reply(request.payload);
+      var message = new Message({
+        message: request.payload.message
+      });
+
+      message.save(function(err, message) {
+        if (err) return console.error(err);
+        console.dir(message);
+      });
+
+      reply({"success": true});
 
     },
     config: {
